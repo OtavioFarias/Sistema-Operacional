b97ba8d98b53049f4fbeebe425b293efb21e0835
Otavio_Farias <otaviogarcia.aluno@unipampa.edu.br>
2025-07-05 15:49:47 -0300

    tabelas organizadas

--- multiThread/testMult.sh
+++ multiThread/testMult.sh
@@ -4,7 +4,7 @@ echo "tamanhoMem,minBloco,requisicoes,threads,cleaningPercent,execucao" > result
 
 for p1 in 1024 2048 4096 8192; do
   for p2 in 2 4 8 16 32 64; do
-    for p3 in 1 10 20 30 40 50 60 100 1000 10000 100000 1000000; do
+    for p3 in 1 10 100 1000 10000 100000 1000000; do
       for p4 in 1 2 4 8 16 32 64; do
         for p5 in 0 10 20 30 40 50 60 70 80 90 100; do
           for p6 in 1; do
